86ebfea214176e5c86c35b797f39e2f3727eca62
Pavel Kuprianov <97345976+PavelKuprianov@users.noreply.github.com>
2022-11-10 06:16:53 +0300

    circle

--- script.js
+++ script.js
@@ -1,43 +1,96 @@
 const canv = document.getElementById('crown');
-console.log('canv: ', canv);
-canv.width = window.innerWidth
-canv.height = window.innerHeight
+canv.width = window.innerWidth / 3 * 2 //ширина игрового поля
+canv.height = window.innerHeight // высота игрового поля
 const ctx = canv.getContext('2d')
 
-let time = 90
-function addArc(x, y, time) {
-
-
-  for (let i = 0; i < time; i++) {
-    setTimeout(() => {
-      ctx.moveTo(x, y);
-
-      ctx.arc(x, y, i, 0, Math.PI * 2)
-      ctx.strokeStyle = 'rgba(243, 22, 22, 0.2)';
-      ctx.stroke()
-    }, (i * 30))
-  }
-
-  setTimeout(() => {
-    const grd = ctx.createRadialGradient(x, y, 0, x, y, time);
-    grd.addColorStop(0, 'rgba(222, 222, 222, 1)');
-    grd.addColorStop(0.5, 'rgba(222, 222, 222, 0.7)');
-    grd.addColorStop(0.8, 'rgba(222, 222, 222, 0.4)');
-    grd.addColorStop(1, 'rgba(219, 15, 15, 0.7)');
-    ctx.beginPath();
-    // ctx.moveTo(x, y);
-
-    ctx.arc(x, y, time, 0, Math.PI * 2)
-    ctx.fillStyle = grd;
-    ctx.fill();
-  }, (time * 30))
+function randomColor() {
+  return Math.floor(Math.random() * (256 - 0 + 1) + 0)
 }
 
-canv.addEventListener('click', (event) => {
-  const x = event.pageX
-  const y = event.pageY
-  addArc(x, y, time)
+//Запуск игры
+const btnStart = document.getElementById('btnStart')
+const radio = document.querySelectorAll('.radio')
+
+btnStart.addEventListener('click', () => {
+  let optionColor
+  radio.forEach((elem => { //Перебираем радиокнопки для определения активной
+    if (elem.checked) {
+      optionColor = elem.id
+    }
+  }))
+  startGame(optionColor)
 })
+
+let time = 0
+let timeFirst = 0
+let timeEnd = 0
+let firstColor, secondColor, thirdColor
+
+function startGame(optionColor) {
+
+  function addArc(x, y, time) {
+
+
+
+    if (optionColor === 'random') {
+      firstColor = randomColor()
+      secondColor = randomColor()
+      thirdColor = randomColor()
+    } else {
+      console.log(optionColor);
+    }
+
+
+    for (let i = 0; i < time; i++) {
+      setTimeout(() => {
+        ctx.beginPath();
+        ctx.strokeStyle = `rgba(${firstColor}, ${secondColor}, ${thirdColor}, 0.5)`;
+        ctx.arc(x, y, i, 0, Math.PI * 2)
+        ctx.fillStyle = `rgba(${firstColor}, ${secondColor}, ${thirdColor}, 0.5)`;
+        ctx.fill();
+        ctx.stroke();
+      }, (i * 30))
+    }
+
+    setTimeout(() => {
+      // ctx.moveTo(x, y);
+      const grd = ctx.createRadialGradient(x, y, 0, x, y, time);
+      ctx.beginPath();
+      grd.addColorStop(0, 'rgba(222, 222, 222, 1)');
+      grd.addColorStop(0.4, 'rgba(222, 222, 222, 0.8)');
+      grd.addColorStop(0.6, 'rgba(222, 222, 222, 0.6)');
+      grd.addColorStop(0.8, 'rgba(222, 222, 222, 0.4)');
+      grd.addColorStop(1, 'rgba(222, 222, 222, 0.3)');
+
+      ctx.arc(x, y, time, 0, Math.PI * 2)
+      ctx.fillStyle = grd;
+      ctx.moveTo(x, y);
+      ctx.fill();
+      ctx.closePath();
+    }, (time * 30 + 0.1))
+
+
+  }
+
+  canv.addEventListener('mousedown', () => {
+    timeFirst = new Date()
+
+
+  })
+
+  canv.addEventListener('click', (event) => {
+    timeEnd = new Date()
+    time = (timeEnd - timeFirst) / 10
+    if (time > 200) { //Устанавливаем ограничения в 200 пикселей на размер шарика
+      time = 200
+    }
+
+    const x = event.pageX
+    const y = event.pageY
+    addArc(x, y, time)
+  })
+
+}
 
 
 
